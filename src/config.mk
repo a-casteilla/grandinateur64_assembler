9a552985ea2d98ab0a0e0c4fd31118e0de300460
Aurélien Casteilla <aurelien.casteilla@gmail.com>
2022-01-08 14:04:25 +0100

    Makefile added

---
+++ src/config.mk
@@ -0,0 +1,13 @@
+# VERSION 0.1
+
+# target
+PREFIX = /usr/local
+MANPREFIX = $(PREFIX)/share/man
+
+# compiler used
+CC = cc
+CFLAGS = -Wall -pedantic -Wextra -std=c99
+
+# debugger options
+DB = gdb
+DBCFLAGS = -ggdb
