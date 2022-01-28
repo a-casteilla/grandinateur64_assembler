10ca4cf80f8d9361df4b6ca552b99746bacc5777
Aurélien Casteilla <aurelien.casteilla@gmail.com>
2022-01-28 09:39:23 +0100

    functions recommented

--- src/align.h
+++ src/align.h
@@ -5,22 +5,32 @@
 
 #define ALIGN_H
 
-/* align the given address on a power of two. 
- * This functions supposes that boundary is a power of two 
- * If not, this function has an undefined behavior
- * This function returns the next address that is aligned or the same address
- * if it's already aligned */
+/* Purpose: align the given address on a power of two. 
+ * Return:  the next address that is aligned or the same address
+ *          if it's already aligned
+ *
+ * address:  can be any uint64_t number
+ * boundary: is supposed to be a power of two.
+ *           If not, this function has an undefined behavior
+ */
 uint64_t align (uint64_t address, uint64_t boundary) ;
 
-/* align the given address on a power of two. 
- * This functions supposes that boundary is a power of two 
- * If not, this function has an undefined behavior
- * This function returns the amount of byte to subtract to each addresses since
- * the following align directive
- * Can be use to check if the code is aligned */
+/* Purpose: align the given address on a power of two, or check if the code is
+ *          aligned.
+ * Return:  the amount of byte to subtract to each addresses since
+ *          the following align directive.
+ *
+ * address:  can be any uint64_t number
+ * boundary: is supposed to be a power of two.
+ *           If not, this function has an undefined behavior
+ */
 uint64_t align_backward (uint64_t address, uint64_t boundary) ;
 
-/* Returns true if nb is a power of two */
+/* Purpose: tell if a uint64_t is a power of two or not
+ * Return:  true is returned if nb is a power of two
+ *
+ * nb: the uint64_t that will be tested.
+ */
 bool is_a_power_of_two (uint64_t nb) ;
 
 #endif // ALIGN_H
