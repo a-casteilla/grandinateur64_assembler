9d43eb6a092f8222fa0c3fc7742843b04704c63e
Aurélien Casteilla <aurelien.casteilla@gmail.com>
2022-01-29 13:07:04 +0100

    comments added

--- src/conv_string.c
+++ src/conv_string.c
@@ -6,9 +6,12 @@
  * input: a string
  */
 /* Additionnal notes:
- * I have added this function because strtoul lacks some features in term of
+ * I have added this function because strtoull lacks some features in term of
  * base recognition. I want to have a '$' as a valid prefix for a hex value and
  * "%" to be a valid binary prefix.
+ * For negative numbers, there is no need to handle them, as long as strtoull
+ * handles them correctly. (It's explained in the man page section 3 of
+ * strtoull)
  */
 uint64_t convert_str_num (char * input) {
     if (*input == '$') {
--- src/conv_string.h
+++ src/conv_string.h
@@ -10,6 +10,9 @@
  * I have added this function because strtoul lacks some features in term of
  * base recognition. I want to have a '$' as a valid prefix for a hex value and
  * "%" to be a valid binary prefix.
+ * For negative numbers, there is no need to handle them, as long as strtoull
+ * handles them correctly. (It's explained in the man page section 3 of
+ * strtoull)
  */
 uint64_t convert_str_num (char * input) ;
 
