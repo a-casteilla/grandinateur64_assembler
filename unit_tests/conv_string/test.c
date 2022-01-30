612870c7020dfee171bdf86b520930aeb6f4c208
Aurélien Casteilla <aurelien.casteilla@gmail.com>
2022-01-30 20:05:44 +0100

    test for conv_string.c finnished

--- unit_tests/conv_string/test.c
+++ unit_tests/conv_string/test.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "../../src/conv_string.h"
 
+/*
+ * Test of the functions :
+ * convert_str_num
+ * convert_literal
+ * 
+ * On the odd lines, escape sequences are converted.
+ * On the even lines, strings are converted to number.
+ * (I assume the line number begins with line 1)
+ */
 int main () {
     char * line = (char *)malloc(BUFSIZ);
+    uint64_t value;
     while (!feof(stdin)) {
         fgets(line, BUFSIZ, stdin);
         if (!feof(stdin)) {
-            printf("\"%s\"\n", line);
+            printf("Original: \n");
+            puts(line);
             convert_literal(&line);
-            printf("\"%s\"\n", line);
+            printf("Converted: \n");
+            puts(line);
+        }
+        fgets(line, BUFSIZ, stdin);
+        if (!feof(stdin)) {
+            printf("Original: \n");
+            puts(line);
+            value = convert_str_num(line);
+            printf("Numeric value: \n");
+            printf("%lx\n", value);
         }
     }
     exit(EXIT_SUCCESS);
