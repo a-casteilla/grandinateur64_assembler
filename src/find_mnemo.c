8366505ca4ec607487bbbdb023925f57946adbc0
Aurélien Casteilla <aurelien.casteilla@gmail.com>
2022-02-04 08:02:12 +0100

    comment added in the header and typo

--- src/find_mnemo.c
+++ src/find_mnemo.c
@@ -6,8 +6,8 @@
  * Return:  error code (0 when everything is fine)
  * Modified input: current_line
  *
- * current_line: a pointer to the last fetched line (the line where we hav to
- *               find the mnemo
+ * current_line: a pointer to the last fetched line (the line where we have to
+ *               find the mnemo)
  */
 int find_mnemo (struct line * current_line) {
     char * mnemonic; /* */
--- src/find_mnemo.h
+++ src/find_mnemo.h
@@ -5,6 +5,14 @@
 #include <string.h>
 #include "common.h"
 
+/* Purpose: This function associates the mnemonic (or directive) of a line with
+ *          its mnemonic number
+ * Return:  error code (0 when everything is fine)
+ * Modified input: current_line
+ *
+ * current_line: a pointer to the last fetched line (the line where we have to
+ *               find the mnemo)
+ */
 int find_mnemo (struct line * current_line) ;
 
 #endif
