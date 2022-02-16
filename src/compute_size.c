236b82065d5e5fa0af58de4ed630835fef7837d9
Aurélien Casteilla <aurelien.casteilla@gmail.com>
2022-02-16 23:00:42 +0100

    compute_size.h recommented

--- src/compute_size.c
+++ src/compute_size.c
@@ -4,11 +4,15 @@
 #include "conv_string.h"
 #include "mnemonic.h"
 
-/* This function tells the size of the input line once assembled */
-/* Return an error code :
- * 0 means no error
- * 1 means an error occured
- * */
+/* Purpose: This function compute the size in byte of a line of the input once
+ *          assembled.
+ * Return:  error code (0 when everything is fine)
+ * Modified input: current_line
+ *
+ * current_line: a pointer to the last fetched line (the line where we have to
+ *               compute the size)
+ */
+/* Note: a warning will still return 0. */
 int compute_size (struct line * current_line) {
     int size = 0;
     switch (mnemo[current_line->mnemo_nb].family) {
