53ac106174a3d8638fa2657095a30bec6c96d87c
Aurélien Casteilla <aurelien.casteilla@gmail.com>
2022-02-19 23:59:01 +0100

    Comments added

--- src/import_symbols.c
+++ src/import_symbols.c
@@ -3,41 +3,61 @@
 /* Returns a pointer to the imported symbols */
 struct symbol * import_symbols (char * filename, uint64_t offset,
         struct scope * scope) {
+
     scope = root_scope(scope); /* Find the root scope because all symbols in
                                   the files belongs to the root scope */
+
+    /* do the memory allocation */
     size_t imported_symbols_size = BUFSIZ;
     struct symbol * imported_symbols = malloc(imported_symbols_size);
+    char line[BUFSIZ];
+
+    /* Initialisation */
     unsigned int nb_of_symbols = 0;
-    char * line = malloc(BUFSIZ);
     char * line_parsed = NULL;
-    FILE * import_file = fopen(filename, "r");
     bool is_label = false;
+
+    /* opens the file */
+    FILE * import_file = fopen(filename, "r");
+
     if (!import_file) {
         fprintf(stderr, "Can't open file \"%s\"\n\n", filename);
         return NULL;
     }
+
     while (!feof(import_file)) {
+        /* By default, the line being fetched contains a constant and not a
+         * label */
         is_label = false;
+        /* the line is being fetched */
         if (!fgets(line, BUFSIZ, import_file) && !feof(import_file)) {
             fprintf(stderr, "An error occured while reading file \"%s\"\n\n",
                     filename);
             return NULL;
         } else if (!feof(import_file)) {
+            /* This is executed if the line has been fetched successfully and
+             * if the file is not ended */
             line_parsed = strtok(line, " \n");
             if (line_parsed) {
+                /* Get the type of the line (label / constant) */
                 /* If the first letter is l then it's a label */
                 if (*line_parsed == 'l') {
                     is_label = true;
                 }
+
                 line_parsed = strtok(NULL, " \n");
+
                 if (line_parsed) {
+                    /* get the offset / value */
                     /* add the numeric of the current line to the table */
                     (imported_symbols + nb_of_symbols)->value
                         = strtoull(line_parsed, NULL, 16);
                     /* add the offset if it's a label */
                     if (is_label)
                         (imported_symbols + nb_of_symbols)->value += offset;
+
                     line_parsed = strtok(NULL, " \n");
+
                     if (line_parsed) {
                         /* add the label's name to the table */
                         (imported_symbols + nb_of_symbols)->name
@@ -45,13 +65,12 @@ struct symbol * import_symbols (char * filename, uint64_t offset,
                         strcpy((imported_symbols + nb_of_symbols)->name,
                                 line_parsed);
                         (imported_symbols + nb_of_symbols)->scope
-                            = scope;
+                            = scope; /* Reminder scope is the root scope */
                         (imported_symbols+nb_of_symbols)->is_label = is_label;
                         nb_of_symbols++;
                         /* alloc more space if necessary */
-                        if (nb_of_symbols
-                                > imported_symbols_size
-                                / sizeof(struct symbol)) {
+                        if ((nb_of_symbols+1) * sizeof(struct symbol)
+                                > imported_symbols_size) {
                             imported_symbols_size += BUFSIZ;
                             imported_symbols = realloc(imported_symbols,
                                     imported_symbols_size);
@@ -63,13 +82,15 @@ struct symbol * import_symbols (char * filename, uint64_t offset,
                 /* No else : just skip the line if there isn't more token
                  * (if the line is invalid) */
             }
+            /* No else : the line is void */
         }
+        /* No else : the end of file has been reached */
     }
     /* The name of the symbol = NULL : end of the symbol list */
     (imported_symbols + nb_of_symbols)->name = NULL;
     nb_of_symbols++;
     /* alloc more space if necessary */
-    if (nb_of_symbols > imported_symbols_size / sizeof(struct symbol)) {
+    if ((nb_of_symbols + 1) * sizeof(struct symbol) > imported_symbols_size) {
         imported_symbols_size += BUFSIZ;
         imported_symbols = realloc(imported_symbols, imported_symbols_size);
     }
