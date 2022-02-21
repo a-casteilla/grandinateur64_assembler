22c4dbc991da9f4b02647618a959c9a02da3242f
Aurélien Casteilla <aurelien.casteilla@gmail.com>
2022-02-21 21:13:22 +0100

    syntax corrected

--- src/def_statement.c
+++ src/def_statement.c
@@ -1,3 +1,4 @@
+#include "mnemonic.h"
 #include "def_statement.h"
 
 /* Purpose: count the number of def directives. This way, the program knows if
@@ -7,10 +8,12 @@
  * lines: the lines of the input file, where def directives have to be counted.
  */
 unsigned int count_def(struct line * lines) {
-    nb_def = 0;
-    for (struct line * l = lines; l->number; l++)
-        if (mnemo[l->mnemo_nb].family == def_directive)
+    unsigned int nb_def = 0;
+    for (struct line * l = lines; l->number; l++) {
+        if (mnemo[l->mnemo_nb].family == def_directive) {
             nb_def++;
+        }
+    }
     return nb_def;
 }
 
